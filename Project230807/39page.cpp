6ac1c4593b02391a37b9c930347a77db0c5f2de4
tlagmlwns <118893467+tlagmlwns@users.noreply.github.com>
2023-08-10 21:57:31 +0900

    Update 39page.cpp

--- Project230807/39page.cpp
+++ Project230807/39page.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> //ÅÛÇÃ¸´ »ç¿ë
-int GetMax(T n1, T n2) { return (n1 > n2) ? n1 : n2; }
+template <typename T> //í…œí”Œë¦¿ í•¨ìˆ˜ ì ìš©ì‹œ
+T GetMax(T n1, T n2) { return (n1 > n2) ? n1 : n2; }
 
 void func4_(){ 
 	cout << GetMax(100, 200) << endl;
